a24d7b4e973a653cc475701c3534d8e5ff3c0057
anjor <anjor@umd.edu>
2022-11-18 11:03:01 +0000

    hating deps

--- pages/api/fetch-html.js
+++ pages/api/fetch-html.js
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer'
-import {Blob} from 'node:buffer';
+import {Blob} from 'node-blob';
 import FormData from "form-data";
 
 export default async function handler(req, res) {
